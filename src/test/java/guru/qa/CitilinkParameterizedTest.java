720e8a35f46673bd4e7b121593bb4accf85caac9
santalovapolina <polinasantalova@yandex.ru>
2023-01-21 22:18:56 +0700

    added numberOfCategories test

--- src/test/java/guru/qa/CitilinkParameterizedTest.java
+++ src/test/java/guru/qa/CitilinkParameterizedTest.java
@@ -1,13 +1,17 @@
 package guru.qa;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CitilinkParameterizedTest {
 
@@ -19,14 +23,21 @@ public class CitilinkParameterizedTest {
 
 
     @CsvFileSource(resources = "/testData.csv")
-
-
     @ParameterizedTest(name = "В результате поиска продукции бренда {0} должен появиться раздел {1}")
     @Tags({@Tag("CRITICAL"), @Tag("UI_TEST")})
     void categorySearchTest(String productBrandName, String productCategory) {
 
         $("input[type=search]").setValue(productBrandName).pressEnter();
         $(".BrandCategories__brand-category-header").$("h2").shouldHave(text(productCategory));
+    }
+
+    @ValueSource(strings = {"apple", "samsung"})
+    @ParameterizedTest(name = "Количество категорий товаров бренда {0} должно быть больше 10")
+    @Tag("UI_TEST")
+    void numberOfCategories(String productBrandName) {
+        $("input[type=search]").setValue(productBrandName).pressEnter();
+        $$("div[class=BrandCategories__brand-category-item]").
+                shouldHave(sizeGreaterThan(10), Duration.ofSeconds(3));
     }
 
 }
