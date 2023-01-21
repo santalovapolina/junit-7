48ec8d954f7685e6e0f5cdcb69c7d0038210d367
santalovapolina <polinasantalova@yandex.ru>
2023-01-21 16:30:48 +0700

    new name for java class

--- src/test/java/guru/qa/CitilinkParameterizedTest.java
+++ src/test/java/guru/qa/CitilinkParameterizedTest.java
@@ -4,13 +4,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class ParameterizedSampleTest {
+public class CitilinkParameterizedTest {
 
     @BeforeEach
     void beforeEach() {
